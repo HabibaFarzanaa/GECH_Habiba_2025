f844f84284fd11d8bc639d25cee50cbeb7eea6b6
HabibaFarzana <habibafarzana764@gmail.com>
2025-05-13 14:38:23 +0530

    First commit

--- EclipseWorkplace.java/springSecurityDemo5/src/main/java/com/springSecurityDemo/springSecurityDemo/config/Securityconfig.java
+++ EclipseWorkplace.java/springSecurityDemo5/src/main/java/com/springSecurityDemo/springSecurityDemo/config/Securityconfig.java
@@ -3,15 +3,19 @@ package com.springSecurityDemo.springSecurityDemo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import com.springSecurityDemo.springSecurityDemo.repository.StudentRepo;
 import com.springSecurityDemo.springSecurityDemo.service.CustomUserDetailsService;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class Securityconfig {
+	
 	private StudentRepo studentRepo;
 
 	public Securityconfig(StudentRepo studentRepo) {
@@ -35,5 +39,26 @@ public class Securityconfig {
 		provider.setPasswordEncoder(passwordEncoder());
 		provider.setUserDetailsService(userDetailsService());
 		return provider;
+	}
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/", "about", "/contact", "/register").permitAll()
+						.anyRequest().authenticated()
+						)
+				.formLogin(login -> login
+						.loginPage("/login")
+						.loginProcessingUrl("/login")
+						.defaultSuccessUrl("/std-details", true)
+						.permitAll()
+						)
+				.logout(logout -> logout
+						.logoutSuccessUrl("/login?logout")
+						.permitAll())
+				.build();
+				
+		
 	}
 }
--- EclipseWorkplace.java/springSecurityDemo5/src/main/java/com/springSecurityDemo/springSecurityDemo/service/StudentService.java
+++ EclipseWorkplace.java/springSecurityDemo5/src/main/java/com/springSecurityDemo/springSecurityDemo/service/StudentService.java
@@ -1,11 +1,12 @@
 package com.springSecurityDemo.springSecurityDemo.service;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import com.springSecurityDemo.springSecurityDemo.DTO.StudentDTO;
 import com.springSecurityDemo.springSecurityDemo.model.Student;
 import com.springSecurityDemo.springSecurityDemo.repository.StudentRepo;
-
+@Service
 public class StudentService {
 	private StudentRepo studentRepo;
 	private PasswordEncoder passwordEncoder;
